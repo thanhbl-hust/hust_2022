a2c561f9d13f598c27c9b95e41c7019325f389c6
thanhbl-hust <lamthanhbui02@gmail.com>
2022-11-30 15:09:54 +0700

    sql

--- KTLT/Buoi_1/1.5.cpp
+++ KTLT/Buoi_1/1.5.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 double* maxValue(double* a, int length){
+	//Bui Lam Thanh 20204606;
 	double* max;
 	max = a; //max = a[0], dia chi co so cua a;
 	if(a == NULL) return NULL;
-	for(int i = 1; i < length; i++){
+	for(int i = 1; i < length; i++){ //di tim gia tri max;
 		if(a[i] > *max){
 			max = &(a[i]);
 		}
@@ -15,12 +16,6 @@ double* maxValue(double* a, int length){
 }
 
 int main(){
-	
-	cout << "Ho va ten: Bui Lam Thanh" << endl;
-	cout << "MSSV: 20204606" << endl;
-	cout << "Phan 2: Con tro va mang" << endl;
-	cout << "----------1.5----------" << endl;
-	
 	double arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	cout << *maxValue(arr, 10) << endl;
 	return 0;
